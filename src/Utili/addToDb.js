c3a5ec060f721325fde6419c8d0bd7aad8717bf9
Your Name <your-email@example.com>
2024-11-03 17:31:38 +0600

    some added

--- src/Utili/addToDb.js
+++ src/Utili/addToDb.js
@@ -1,21 +1,21 @@
 const getStoreReadList = () => {
-    const storedListStr = localStorage.getItem("read-list");
-    if (storedListStr) {
-        return JSON.parse(storedListStr);
-    }
-    return [];
+  const storedListStr = localStorage.getItem("read-list");
+  if (storedListStr) {
+    return JSON.parse(storedListStr);
+  }
+  return [];
 };
 
 const addToStoreReadList = (id) => {
-    const storeList = getStoreReadList();
-    if (storeList.includes(id)) {
-        console.log(id, "already exists");
-    } else {
-        storeList.push(id);
-        const storedListStr = JSON.stringify(storeList);
-        localStorage.setItem("read-list", storedListStr);
-        console.log("Updated read-list in localStorage:", storeList);
-    }
+  const storeList = getStoreReadList();
+  if (storeList.includes(id)) {
+    console.log(id, "already exists");
+  } else {
+    storeList.push(id);
+    const storedListStr = JSON.stringify(storeList);
+    localStorage.setItem("read-list", storedListStr);
+    console.log("Updated read-list in localStorage:", storeList);
+  }
 };
 
 export { addToStoreReadList };
